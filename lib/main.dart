c449534137848a86269cbb7d1b69fa83efacf576
rtcteen <rtcteen30@gmail.com>
2025-02-23 23:45:52 +0700

    homework3

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'week2',
+      title: 'week5',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
